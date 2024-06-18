f50a741fb2f860de59b5717c590038bfaaf2b1bf
Nicolò Alagna <155567338+mem3nto0@users.noreply.github.com>
2024-06-18 08:58:44 +0200

    Update DLRN_GUI.py

--- DLRN_GUI.py
+++ DLRN_GUI.py
@@ -100,31 +100,62 @@ class MyWindow(QMainWindow):
         
         if selected_option == "Spectra":
             path = os.getcwd()
-            path_model = path + "/pretrained_model_Spectra/resV2_spectra_model"
-            model_model = tf.keras.models.load_model(path_model, custom_objects={'top_3_acc':top_3_acc})
-            Global_variable.model_model = model_model
 
-            path_amp = path + "/pretrained_model_Spectra/resV2_spectra_amplitude"
-            model_amp = tf.keras.models.load_model(path_amp)
-            Global_variable.model_amp = model_amp
+            try:
+                path_model = path + "/pretrained_model/Spectra/resV2_spectra_model"
+                model_model = tf.keras.models.load_model(path_model, custom_objects={'top_3_acc':top_3_acc})
+                Global_variable.model_model = model_model
 
-            path_tau = path + "/pretrained_model_Spectra/resV2_spectra_Tau"
-            model_tau = tf.keras.models.load_model(path_tau)
-            Global_variable.model_tau = model_tau
+                path_amp = path + "/pretrained_model/Spectra/resV2_spectra_amplitude" # 
+                model_amp = tf.keras.models.load_model(path_amp)
+                Global_variable.model_amp = model_amp
+
+                path_tau = path + "/pretrained_model/Spectra/resV2_spectra_Tau"
+                model_tau = tf.keras.models.load_model(path_tau)
+                Global_variable.model_tau = model_tau
+
+            except:
+                path_model = path + "/pretrained_model/pretrained_model/Spectra/resV2_spectra_model"
+                model_model = tf.keras.models.load_model(path_model, custom_objects={'top_3_acc':top_3_acc})
+                Global_variable.model_model = model_model
+
+                path_amp = path + "pretrained_model//pretrained_model/Spectra/resV2_spectra_amplitude" # 
+                model_amp = tf.keras.models.load_model(path_amp)
+                Global_variable.model_amp = model_amp
+
+                path_tau = path + "pretrained_model//pretrained_model/Spectra/resV2_spectra_Tau"
+                model_tau = tf.keras.models.load_model(path_tau)
+                Global_variable.model_tau = model_tau
 
         if selected_option == "Agarose Gel": # to change for the GEL path
             path = os.getcwd()
-            path_model = path + "/pretrained_model_Gel/resV2_GEL_takemodel"
-            model_model = tf.keras.models.load_model(path_model, custom_objects={'top_3_acc':top_3_acc})
-            Global_variable.model_model = model_model
 
-            path_amp = path + "/pretrained_model_Gel/resV2_GEL_takespectra+dilat"
-            model_amp = tf.keras.models.load_model(path_amp)
-            Global_variable.model_amp = model_amp
+            try:
+                path_model = path + "/pretrained_model/Gel/resV2_GEL_takemodel"
+                model_model = tf.keras.models.load_model(path_model, custom_objects={'top_3_acc':top_3_acc})
+                Global_variable.model_model = model_model
 
-            path_tau = path + "/pretrained_model_Gel/resV2_GEL_Tau(MSE)"
-            model_tau = tf.keras.models.load_model(path_tau)
-            Global_variable.model_tau = model_tau
+                path_amp = path + "/pretrained_model/Gel/resV2_GEL_takespectra+dilat"
+                model_amp = tf.keras.models.load_model(path_amp)
+                Global_variable.model_amp = model_amp
+
+                path_tau = path + "/pretrained_model/Gel/resV2_GEL_Tau(MSE)"
+                model_tau = tf.keras.models.load_model(path_tau)
+                Global_variable.model_tau = model_tau
+
+            except:
+
+                path_model = path + "pretrained_model//pretrained_model/Gel/resV2_GEL_takemodel"
+                model_model = tf.keras.models.load_model(path_model, custom_objects={'top_3_acc':top_3_acc})
+                Global_variable.model_model = model_model
+
+                path_amp = path + "pretrained_model//pretrained_model/Gel/resV2_GEL_takespectra+dilat"
+                model_amp = tf.keras.models.load_model(path_amp)
+                Global_variable.model_amp = model_amp
+
+                path_tau = path + "pretrained_model//pretrained_model/Gel/resV2_GEL_Tau(MSE)"
+                model_tau = tf.keras.models.load_model(path_tau)
+                Global_variable.model_tau = model_tau
 
         Global_variable.Kind_analysis = selected_option
 
